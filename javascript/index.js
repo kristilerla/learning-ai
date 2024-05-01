83b42626a1c61bb1f7b2caa299710a96dc721dc9
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-01 14:54:46 +0200

    updated prompt

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ function showResult(response){
 
 let apiKey = "4ff060ct0o35c409303b648f04f0ab31";
 let context = "be polite, inventive, and provide a very short answer";
-let prompt = "who was the first female president in the world?";
+let prompt = "who was the first female president in the world and when was she president?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 console.log("prosessing");
