f1fe2c8fcb990ae2cb7532058d4bd05abc4e2206
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-03 18:00:35 +0200

    updated quotes

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,30 @@
-function handleClick (response){
-    alert(response.data.answer)
+function displayQuote (response){
+    console.log(response.data.answer);
     
+    new Typewriter("#quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20
+  });
 }
 
-let buttonElement = document.querySelector("#special-button");
-buttonElement.addEventListener("click", handleClick);
-
-function showResult(response) {
-   alert (response.data.answer)
-}
+function generateQuote(event) {
+    event.preventDefault();
 
 let apiKey = "4ff060ct0o35c409303b648f04f0ab31";
 let context = "be Yoda from Starwars and provide a good answer";
 let prompt = "giv me a creativ quote that Yoda would have given";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+let quoteElement = document.querySelector("#quote");
+quoteElement.innerHTML = "A quote for you we are generating.."
+
 console.log("prosessing");
-axios.get(apiUrl) .then (showResult);
+axios.get(apiUrl) .then (displayQuote);
+}
+let generatorButton = document.querySelector("#special-button");
+generatorButton.addEventListener("click",generateQuote);
+
 
 
