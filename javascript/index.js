e14d5b3af153e5d8f542da08554c9acae7513a47
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-03 17:05:55 +0200

    building a Yoda quote generator

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,28 @@
+function handleClick(response){
+    return(response.data.answer)
+    
+}
+
+let buttonElement = document.querySelector("#special-button");
+buttonElement.addEventListener("click", handleClick);
+
+new Typewriter('#response', {
+  strings: ['return'],
+  autoStart: true,
+});
+
+
+
 function showResult(response){
     alert(response.data.answer);
 }
 
 let apiKey = "4ff060ct0o35c409303b648f04f0ab31";
-let context = "be polite, inventive, and provide a very short answer";
-let prompt = "who was the first female president in the world and when was she president?";
+let context = "be Yoda from Starwars and provide a good answer";
+let prompt = "giv me a creativ quote that Yoda would have given";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 console.log("prosessing");
 axios.get(apiUrl) .then (showResult);
+
 
